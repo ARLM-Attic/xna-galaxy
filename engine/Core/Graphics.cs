af99e6fd94100383a94ded9d9ba71d2033d4f49c
SND\anecia_cp <SND\anecia_cp@a83551a4-30f6-4d81-a974-c6ced450ddbf>
2009-02-08 16:08:06 +0000

    함수설명 업데이트 등의 마이너한 업데이트 입니다. Plane 명칭보단 Layer 명칭이 나은거 같아 Layer로 변경 하였습니다.
    git-svn-id: https://galaxy.svn.codeplex.com/svn@9773 a83551a4-30f6-4d81-a974-c6ced450ddbf

--- engine/Core/Graphics.cs
+++ engine/Core/Graphics.cs
@@ -1,10 +1,14 @@
 ﻿/**
  * Graphics.cs
  * 
- * 2D / 3D Graphics Implementation
+ * 2D / 3D Graphics Implementation.
  * 
  * @file
  * @author
+ *        2D Part: YongChul [Chris] Jin / id: anecia
+ *                 [Please remove this line and put your name/id here instead]
+ *        3D Part:
+ *
  * Copyright (C) XNA Naver cafe, 2009. All rights reserved.
  * $Id: $
  */
@@ -22,16 +26,20 @@ using System.Diagnostics;
 
 namespace Galaxy.Core
 {
-    public class Sprite
+    struct Image2D
     {
+        public UInt32      id;
+        public Vector2     position;
+        public Texture2D   image;
+        public bool        holdLife;
     };
 
     public class Graphics
     {
         private GraphicsDeviceManager grpDevMgr   = null;
         private SpriteBatch           sprBatch    = null;
-        private LinkedList<Sprite>[]  planes      = null;
-        private uint                  maxPlaneNum = 0;
+        private LinkedList<Image2D>[] layers      = null;
+        private uint                  maxLayerNum = 0;
 
         //
         // Summary:
@@ -43,29 +51,57 @@ namespace Galaxy.Core
 
         public Graphics()
         {
-            Debug.Fail("Please call Graphics(game) constructor "
+            Debug.Fail("Please call Graphics(game, planeNum) constructor "
                          + "instead of this constructor"); 
         }
-        public Graphics(Game game, uint planeNum)
+        
+        /**
+         * Graphics class constructor.
+         * 
+         * Initializes a new instance of this class, which provides
+         * GraphicsDeviceManager and image layer.
+         * 
+         * @param game          Game object
+         * @param layerNum      The number of image layers
+         */
+        public Graphics(Game game, uint layerNum)
         {
             Debug.Assert(game != null);
 
             grpDevMgr = new GraphicsDeviceManager(game);
 
-            planes = new LinkedList<Sprite>[planeNum];
-            Debug.Assert(planes != null);
-            if ( planes != null )
+            layers = new LinkedList<Image2D>[layerNum];
+            Debug.Assert(layers != null);
+            if ( layers != null )
             {
-                maxPlaneNum = planeNum;
+                maxLayerNum = layerNum;
             }
         }
 
+        /**
+         * Sets the GraphicsDevice.
+         * 
+         * @param grpDevice     Graphics Device
+         */
         public void SetGraphicDevice(GraphicsDevice grpDevice)
         {
             GraphicsDevice = grpDevice;
             Debug.Assert(GraphicsDevice != null);
         }
-
+        
+        //===================================================================
+        // 2D Graphic Implimentation
+        //===================================================================
+        #region 2DGraphic
+        
+        /**
+         * Initializes 2D Graphic system.
+         * 
+         * It should be called once before using 2D Graphic methods.
+         * 
+         * @return  true if the initialization of 2D system is succeed,
+         *          false otherwise.
+         */
         public bool Init2D()
         {
             Debug.Assert(GraphicsDevice != null);
@@ -75,6 +111,11 @@ namespace Galaxy.Core
             return (sprBatch != null);
         }
 
+        /**
+         * Clears the screen with the given color.
+         * 
+         * @param color     Color to clear the screen
+         */
         public void ClearScreen(Color color)
         {
             Debug.Assert(GraphicsDevice != null);
@@ -82,11 +123,43 @@ namespace Galaxy.Core
         }
 
         /**
-         * 2D Graphics
+         * Puts the image to an image layer.
+         * 
+         * The image added to the image layer is not drawn immediately,
+         * it will be drawn when Update() method is called.
+         * 
+         * @param layerNo       Layer number which will have the image
+         * @param image         Image, 2D texture
+         * @param position      The location, in screen coordinates,
+         *                      where the image will be put.
+         * 
+         * @return  an unique id for the added image as Image2D to an
+         *          image layer, or 0 otherwise.
          */
-        void DrawImage(uint planeNo, Texture2D image, Vector2 position)
+        UInt32 PutImage(uint layerNo, Texture2D image, Vector2 position)
         {
-            Debug.Assert(planeNo < maxPlaneNum);
+            Image2D myImage;
+
+            Debug.Assert(layerNo < maxLayerNum);
+
+            myImage = new Image2D();
+            myImage.id       = 1;
+            myImage.position = position;
+            myImage.image    = image;
+            myImage.holdLife = true;
+
+            layers[layerNo - 1].AddLast(myImage);
+
+            return myImage.id;
         }
+        
+        #endregion // 2DGraphic
+
+        //===================================================================
+        // 3D Graphic Implimentation
+        //===================================================================
+        #region 2DGraphic
+    
+        #endregion // 3DGraphic
     };
 }
