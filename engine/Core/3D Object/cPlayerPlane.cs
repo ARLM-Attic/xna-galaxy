015ca63b16a739fb868dbcd147cc993a3d90ec5a
SND\rucifer1217_cp <SND\rucifer1217_cp@a83551a4-30f6-4d81-a974-c6ced450ddbf>
2009-03-23 14:39:43 +0000

    cPlayerPlane.cs 의 주석 첨가
    git-svn-id: https://galaxy.svn.codeplex.com/svn@14698 a83551a4-30f6-4d81-a974-c6ced450ddbf

--- engine/Core/3D Object/cPlayerPlane.cs
+++ engine/Core/3D Object/cPlayerPlane.cs
@@ -21,16 +21,16 @@ namespace Galaxy.Core
         public Vector3 position = new Vector3(0,25,0); // 임시로 보이게 하기 위해 높게 띄워놈.
         public Vector3 rotation = Vector3.Zero;
 
-        public float scale = 0.005f;
-        public float rotation_rate = 0.05f;
-        public float velocity = 0.0f;
+        public float scale = 0.005f;          //크기비율
+        public float rotation_rate = 0.05f;         //회전
+        public float velocity = 0.0f;           //속도
         public float acceleration = 0.01f;      // 가속도
         public float friction = 0.0025f;           // 마찰
         public float max_veloctiy = 250.0f;     // 최대 속도
         
-        public Vector3 direction = Vector3.Zero;
-        public Vector3 up = Vector3.Up;
-
+        public Vector3 direction = Vector3.Zero;      //진행방향
+        public Vector3 up = Vector3.Up;        //윗방향
+         
         private Matrix cameraProjectionMatrix;
         private Matrix cameraViewMatrix;
 
@@ -60,37 +60,37 @@ namespace Galaxy.Core
             cameraProjectionMatrix = projMat;
             cameraViewMatrix = viewMat;
 
-            if (Input.IsKeyDown(Keys.Left))
+            if (Input.IsKeyDown(Keys.Left))         //왼쪽키 눌렸을때
             {
-                rotation.X += rotation_rate;
-                rotation.Z += rotation_rate;
-                if (rotation.Z > MathHelper.PiOver2)
+                rotation.X += rotation_rate;     //반시계방향으로 회전
+                rotation.Z += rotation_rate;    //기체가 왼쪽으로 기움
+                if (rotation.Z > MathHelper.PiOver2)   //기울기 정도를 제한
                     rotation.Z = MathHelper.PiOver2;
             }
             else if (Input.IsKeyDown(Keys.Right))
             {
-                rotation.X -= rotation_rate;
-                rotation.Z -= rotation_rate;
-                if (rotation.Z < -MathHelper.PiOver2)
+                rotation.X -= rotation_rate;        //시계방향으로 회전
+                rotation.Z -= rotation_rate;          //기체가 오른쪽으로 기움
+                if (rotation.Z < -MathHelper.PiOver2)   //기울기 정도를 제한
                     rotation.Z = -MathHelper.PiOver2;
             }
-            else if (Input.IsKeyDown(Keys.Up))
+            else if (Input.IsKeyDown(Keys.Up))       
             {
-                rotation.Y += rotation_rate;
+                rotation.Y += rotation_rate;      //기체의 방향으로 위쪽으로
             }
-            else if (Input.IsKeyDown(Keys.Down))
+            else if (Input.IsKeyDown(Keys.Down))     //기체의 방향으로 아래쪽으로
             {
-                rotation.Y -= rotation_rate;
+                rotation.Y -= rotation_rate;    
             }
-            else
-            {
-                //아무 키 안누르면 다시 원각도로 돌아옮.
+            else   //아무 키 안누르면 다시 기울기정도가 돌아옮.
+            {         
                 if (rotation.Z > 0)
                     rotation.Z -= rotation_rate;
                 if (rotation.Z < 0)
                     rotation.Z += rotation_rate;
             }
 
+            #region rotation simplification
             if (rotation.X > MathHelper.TwoPi)
                 rotation.X -= MathHelper.TwoPi;
             if (rotation.X < -MathHelper.TwoPi)
@@ -102,20 +102,21 @@ namespace Galaxy.Core
             if (rotation.Z > MathHelper.TwoPi)
                 rotation.Z -= MathHelper.TwoPi;
             if (rotation.Z < -MathHelper.TwoPi)
-                rotation.Z += MathHelper.TwoPi;
+                rotation.Z += MathHelper.TwoPi; 
+            #endregion      //rotation의 범위 제한
 
 
             #region Move
             
-            Vector3 vOrg = new Vector3(0, 0, -1);
-            Vector3 vOrg_up = new Vector3(0, 1, 0);
+            Vector3 vOrg = new Vector3(0, 0, -1);   //기체의 초기 진행방향
+            Vector3 vOrg_up = new Vector3(0, 1, 0); //기체의 초기 윗방향
             if (Input.IsKeyDown(Keys.Space))
             {
                 TimeSpan timespan = new TimeSpan(0, 0, 0, 0, 500);  // 0.5초
                 if (gameTime.ElapsedGameTime < timespan)            // 0.5초이내에 다시 스페이스를 누른다면?
                 {
-                    velocity += acceleration;
-                    if (velocity > max_veloctiy)
+                    velocity += acceleration;      //점차 가속도로 인해 속도 증가
+                    if (velocity > max_veloctiy)  //속도 제한
                     {
                         velocity = max_veloctiy;
                     }
@@ -123,8 +124,8 @@ namespace Galaxy.Core
             }
             else //아닐떄;
             {
-                velocity -= friction;
-                if (velocity < 0.0f)
+                velocity -= friction;   //점차 가속도록 속도 감소
+                if (velocity < 0.0f)      //속도 제한
                 {
                     velocity = 0.0f;
                 }
